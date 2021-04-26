1a866aaf514dec9c38844f2c3577e3ad23cb0f09
Stefania9797 <stefaniamastrel@gmail.com>
2021-04-26 12:04:42 +0200

    correction

--- assets/main.js
+++ assets/main.js
@@ -1,27 +1,27 @@
 //chiedere all'utente il numero di chilometri che vuole percorrere 
-var distanza = parseInt(prompt("Quanti chilometri devi percorrere?"));
+var distanza = Number(prompt("Quanti chilometri devi percorrere?"));
 
 //se l'utente non inserisce un numero
 if(isNaN(distanza)) {
     alert("Inserisci un valore valido!")
 }
 // chiedere l'età del passeggero.
-var età = parseInt(prompt("Quanti anni hai?"));
+var età = Number(prompt("Quanti anni hai?"));
 
 //se l'utente non inserisce un numero
 if(isNaN(età)) {
     alert("Inserisci un età valida!")
 }
 //il prezzo del biglietto, con al massimo 2 cifre decimali
-var prezzo = distanza * 0.21.toFixed(2);
+var prezzo = Number(distanza * 0.21).toFixed(2);
 
 //sconto età
 if (età < 18)  {
     //sconto 20% minorenni
-    prezzo = (prezzo - (prezzo * .2)).toFixed(2);
+    prezzo = Number(prezzo - (prezzo * .2)).toFixed(2);
   } else if (età > 65) {
       //sconto 40% over 65
-    prezzo = (prezzo - (prezzo * .4)).toFixed(2);
+    prezzo = Number(prezzo - (prezzo * .4)).toFixed(2);
   }
 
   var messaggio = document.getElementById("prezzo_finale");
